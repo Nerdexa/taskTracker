a1a7ffac0a51af14be4dd09576992b1c64ee7fd5
KARO250827 <masahiro.k1009@gmail.com>
2023-09-10 02:20:09 +0900

    learn: js and sass tutorial

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function main() {}
+// TODO1:入力フォームにテキストが書き込まれて「enter」ボタンが押されると検証ツールのコンソールにテキストが表示される
+// TODO2: enterが押されると入力されたテキストがTODO Listの中に追加される
+// TODO3: TODO Listにテキストが追加されたら入力フォームの中身は空になる
+// TODO4: 入力フォームが値が空の場合はボタンは押せないようにする
+// TODO5: TODOの「Done」ボタンが押されたら、そのTODOは一覧から削除される
+
+function alertButton() {
+  alert('アラートです');
+}
+
+function main() {
+  console.log('jsが読み込まれています');
+
+  // NOTE: constは定数。letは変数
+  const alertElement = 'alertElement';
+  let alertElement2 = 'alertElement2';
+
+  const buttonElement = document.getElementById('alert');
+  buttonElement.addEventListener('click', alertButton);
+}
 
 main();
