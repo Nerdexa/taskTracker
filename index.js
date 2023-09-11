e0abe4173932dc0d72f1b7ea6913ffbacde7030a
KARO250827 <masahiro.k1009@gmail.com>
2023-09-11 22:59:25 +0900

    add: creating todo list

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// TODO1:入力フォームにテキストが書き込まれて「enter」ボタンが押されると検証ツールのコンソールにテキストが表示される
-// TODO2: enterが押されると入力されたテキストがTODO Listの中に追加される
 // TODO3: TODO Listにテキストが追加されたら入力フォームの中身は空になる
 // TODO4: 入力フォームが値が空の場合はボタンは押せないようにする
 // TODO5: TODOの「Done」ボタンが押されたら、そのTODOは一覧から削除される
@@ -8,15 +6,46 @@ function alertButton() {
   alert('アラートです');
 }
 
+// 関数：コンソールへの入力テキストの出力、リストへの入力テキストの追加
+function addItem() {
+  const getText = document.getElementById('text');
+  const inputValue = getText.value;
+
+  if (inputValue.trim() !== '') {
+    console.log(inputValue);
+    const createTodo = document.createElement('li');
+    createTodo.classList.add('todo');
+
+    const createTodoTitle = document.createElement('span');
+    createTodoTitle.classList.add('todo-title');
+    createTodoTitle.textContent = inputValue;
+    createTodo.appendChild(createTodoTitle);
+
+    const createTodoButton = document.createElement('button');
+    createTodoButton.classList.add('todo-button');
+    createTodoButton.textContent = 'Done';
+    createTodo.appendChild(createTodoButton);
+
+    const addList = document.getElementById('itemList');
+    itemList.appendChild(createTodo);
+
+    getText.value = '';
+  }
+}
+
 function main() {
   console.log('jsが読み込まれています');
 
-  // NOTE: constは定数。letは変数
-  const alertElement = 'alertElement';
-  let alertElement2 = 'alertElement2';
-
-  const buttonElement = document.getElementById('alert');
-  buttonElement.addEventListener('click', alertButton);
+  // TODO1:入力フォームにテキストが書き込まれて「enter」ボタンが押されると検証ツールのコンソールにテキストが表示される
+  // TODO2: enterが押されると入力されたテキストがTODO Listの中に追加される
+  const buttonElement = document.getElementById('enter');
+  buttonElement.addEventListener('click', addItem);
 }
 
 main();
+
+// NOTE: constは定数。letは変数
+// const alertElement = 'alertElement';
+// let alertElement2 = 'alertElement2';
+// const buttonElement = document.getElementById('alert');
+// buttonElement.addEventListener('click', alertButton);
